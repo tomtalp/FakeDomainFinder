4bc7550b0f51844103653607b519c5bc01061407
tomtalp <tomtalp93@gmail.com>
2020-01-03 16:02:33 +0200

    random generation

--- main.py
+++ main.py
@@ -45,8 +45,8 @@ class FakeDomainGenerator(object):
 	def __init__(self):
 		self.permutations_dictionay = {
 			"a": ["4"],
-			"b": ["d"],
-			"d": ["b"],
+			# "b": ["d"],
+			# "d": ["b"],
 			"e": ["3"],
 			"i": ["l", "1"],
 			"l": ["i", "1"],
@@ -63,12 +63,41 @@ class FakeDomainGenerator(object):
 		seed = seed.lower()
 		seed_with_http = "http://" + seed
 		seed_domain_after_tld_parsing = tld_parser.parse_tld(seed_with_http)
-		tld, domain, subdomain = seed_domain_after_tld_parsing
-		# print(tld, domain, subdomain)
-		permutable_chars_in_word = [c for c in domain if c in self.permutations_dictionay]
-		print("permutable_chars_in_word = ", permutable_chars_in_word)
+		tld, domain, _ = seed_domain_after_tld_parsing # ignoring subdomain for now
 
-f = FakeDomainGenerator()
-f.get_fake_domain("bluevoyant.com")
+		permutable_chars_in_word = [c for c in domain if c in self.permutations_dictionay]
+		if len(permutable_chars_in_word) == 1:
+			n_of_obfuscations = 1
+		else:
+			n_of_obfuscations = np.random.randint(1, len(permutable_chars_in_word))
+
+		chars_to_obfuscate = np.random.choice(permutable_chars_in_word, n_of_obfuscations, replace=False)
+		# print("chars_to_obfuscate = ", chars_to_obfuscate)
+
+		new_domain = ""
+		for c in domain:
+			if c in chars_to_obfuscate:
+				new_char = np.random.choice(self.permutations_dictionay[c], 1)[0]
+				new_domain += new_char
+			else:
+				new_domain += c
+		
+		return "{new_domain}.{tld}".format(new_domain=new_domain, tld=tld)
+		
+
+fake_generator = FakeDomainGenerator()
+
+fake_domains = {}
+for domain in base_domains:
+	if domain not in fake_domains:
+		fake_domains[domain] = set()
+	
+	for i in range(5):
+		randomly_generated_domain = fake_generator.get_fake_domain(domain)
+		fake_domains[domain].add(randomly_generated_domain)
+
+print(fake_domains)
+# fake_domain = f.get_fake_domain("bluevoyant.com")
+# print("fake_domain = " + fake_domain)
 # text2ndarray("bluevoyant.com")
 
